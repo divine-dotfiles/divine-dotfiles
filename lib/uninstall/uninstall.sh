651d30218d54d778be446c0881247087117953eb
Grove Pyree <grayarea@protonmail.ch>
2019-05-12 20:07:16 +0300

    uninstall.sh: rewrite with opts and better output

--- lib/uninstall/uninstall.sh
+++ lib/uninstall/uninstall.sh
@@ -4,23 +4,28 @@ main()
   # Colorize output
   __declare_global_colors
 
-  # Status variable
-  local removal_successful=false
+  # Parse arguments
+  __parse_arguments "$@"
 
   # Main removal
   if __locate_installations; then
+
     # Erase Divine.dotfiles directory
     if __erase_d_dir; then
+
       # Also, remove shortcut command, if it is present
       __uninstall_shortcut
+
       # Report success
-      printf >&2 '\n%s %s\n' "${BOLD}${GREEN}==>${NORMAL}" 'Glowing success'
+      report_success 'All done'
       return 0
+
     fi
+
   fi
 
   # Report failure
-  printf >&2 '\n%s %s\n' "${BOLD}${RED}==>${NORMAL}" 'Crippling failure'
+  report_failure 'Nothing was removed'
   return 1
 }
 
@@ -33,6 +38,7 @@ __declare_global_colors()
     RED="$( tput setaf 1 )"
     GREEN="$( tput setaf 2 )"
     YELLOW="$( tput setaf 3 )"
+    CYAN="$( tput setaf 6 )"
     WHITE="$( tput setaf 7 )"
     BOLD="$( tput bold )"
     REVERSE="$( tput rev )"
@@ -41,6 +47,7 @@ __declare_global_colors()
     RED=''
     GREEN=''
     YELLOW=''
+    CYAN=''
     WHITE=''
     BOLD=''
     REVERSE=''
@@ -48,41 +55,68 @@ __declare_global_colors()
   fi
 }
 
+__parse_arguments()
+{
+  # Define global storage for option values
+  D_QUIET=false           # Be verbose by default
+  D_REMOVE_ALL=           # Whether to perform removal
+
+  # Extract arguments passed to this script (they start at $0)
+  local args=( "$0" "$@" ) arg
+
+  # Parse arguments
+  for arg in "${args[@]}"; do
+    case "$arg" in
+      --quiet)            D_QUIET=true;;
+      --verbose)          D_QUIET=false;;
+      --yes)              D_REMOVE_ALL=true;;
+      --no)               D_REMOVE_ALL=false;;
+      *)                  :;;
+    esac
+  done
+}
+
 __locate_installations()
 {
   # Try the usual installation directory unless overridden
-  [ -n "$D_DIR" ] || D_DIR="$HOME/.divine"
+  [ -n "$D_INSTALL_PATH" ] || D_INSTALL_PATH="$HOME/.divine"
+  debug_print "Installation directory: $D_INSTALL_PATH"
 
   # Rely on existence of ‘intervene.sh’ within the dir
-  [ -f "${D_DIR}/intervene.sh" ] || {
-    printf >&2 '\n%s %s\n  %s\n' \
-      "${BOLD}${RED}==>${NORMAL}" \
-      "Failed to recognize Divine.dotfiles insatllation directory at:"
-      "$D_DIR"
+  if [ ! -e "$D_INSTALL_PATH" ]; then
+    debug_print 'Installation directory does not exist; nothing to remove'
     return 1
-  }
+  elif [ -f "$D_INSTALL_PATH" ]; then
+    debug_print 'Installation directory is a file; refusing to touch'
+    return 1
+  elif [ ! -e "$D_INSTALL_PATH/intervene.sh" ]; then
+    debug_print 'Installation directory does not resemble Divine.dotfiles'
+    return 1
+  fi
   
   # Storage variables
-  local shortcut_memo_path="$D_DIR/lib/uninstall/shortcut-location"
+  local shortcut_memo_path="$D_INSTALL_PATH/lib/uninstall/shortcut-location"
   local shortcut_filepath
 
+  # Set global variable
+  D_SHORTCUT_FILEPATHS=()
+
+  # Extract user-provided shortcut path
+  [ -n "$D_SHORTCUT_FILEPATH" ] \
+    && __check_shortcut_filepath "$D_SHORTCUT_FILEPATH" \
+    && D_SHORTCUT_FILEPATHS+=( "$D_SHORTCUT_FILEPATH" )
+
   # Try to figure location of shortcut command
-  if [ -r "$shortcut_memo_path" ]; then
+  if [ -r "$shortcut_memo_path" -a -f "$shortcut_memo_path" ]; then
 
-    # Read stored memo
-    shortcut_filepath="$( cat "$shortcut_memo_path" | head -1 )"
+    # Read stored memo line by line
+    while IFS="" read -r shortcut_filepath || [ -n "$shortcut_filepath" ]; do
+      
+      # Check shortcut and add to global variable
+      __check_shortcut_filepath "$shortcut_filepath" \
+        && D_SHORTCUT_FILEPATHS+=( "$shortcut_filepath" )
 
-    # Ensure the shortcut path exists and is a symlink
-    [ -x "$shortcut_filepath" -a -L "$shortcut_filepath" ] || return 0
-
-    # Ensure the link points to ‘intervene.sh’ (if readlink is available)
-    if command -v readlink &>/dev/null; then
-      [ "$( readlink -- "$shortcut_filepath" )" = "$D_DIR/intervene.sh" ] \
-        || return 0
-    fi
-
-    # Set global variable
-    D_SHORTCUT_FILEPATH="$shortcut_filepath"
+    done <"$shortcut_memo_path"
     
   fi
 
@@ -90,47 +124,145 @@ __locate_installations()
   return 0
 }
 
-__uninstall_shortcut()
+__check_shortcut_filepath()
 {
-  # Check if shortcut is detected and exists as a symlink
-  [ -L "$D_SHORTCUT_FILEPATH" ] || return 0
+  # Extract shortcut filepath
+  local shortcut_filepath="$1"
 
-  # Remove shortcut, using sudo if need be
-  if [ -w "$( dirname -- "$D_SHORTCUT_FILEPATH" )" ]; then
-    rm -f "$D_SHORTCUT_FILEPATH"
-  else
-    sudo rm -f "$D_SHORTCUT_FILEPATH"
-  fi
-  
-  # Check if removal went fine
-  [ $? -eq 0 ] || {
-    printf >&2 '\n%s %s\n  %s\n' \
-      "${BOLD}${RED}==>${NORMAL}" \
-      'Failed to uninstall shortcut command at:' \
-      "$D_SHORTCUT_FILEPATH"
+  # Ensure the shortcut path exists and is a symlink
+  [ -L "$shortcut_filepath" ] || {
+    debug_print "Skipping shortcut filepath: $shortcut_filepath" \
+      'Not a symlink'
     return 1
   }
 
-  # Report success
-  printf >&2 '\n%s %s\n  %s\n' \
-    "${BOLD}${GREEN}==>${NORMAL}" \
-    'Successfully uninstalled shortcut command at:' \
-    "$D_SHORTCUT_FILEPATH"
-  return 0
+  # Ensure the link points to ‘intervene.sh’ (if readlink is available)
+  if command -v readlink &>/dev/null; then
+    [ "$( readlink -- "$shortcut_filepath" )" \
+      = "$D_INSTALL_PATH/intervene.sh" ] \
+        || {
+          debug_print "Skipping shortcut filepath: $shortcut_filepath" \
+            'Not pointing to intervene.sh'
+          return 1
+        }
+  fi
 }
 
 __erase_d_dir()
 {
+  # Store long-ass reference in digestible name
+  local name="${BOLD}Divine.dotfiles${NORMAL}"
+
   # Offer to uninstall framework
+  if prompt "$D_REMOVE_ALL" 'Uninstall?' \
+    "${name} Bash framework installed at:" \
+    "$D_INSTALL_PATH"
+  then
+    report_start "Uninstalling ${name}"
+  else
+    report_skip "Refused to uninstall ${name}"
+    return 1
+  fi
+
+  # Straight-forward enough
+  rm -rf "$D_INSTALL_PATH" || {
+    debug_print "Failed to erase directory at: $D_INSTALL_PATH"
+    return 1
+  }
+
+  # Report success
+  report_success "Successfully uninstalled ${name} Bash framework from:" \
+    "$D_INSTALL_PATH"
+  return 0
+}
+
+__uninstall_shortcut()
+{
+  # Iterate over detected shortcut paths
+  local shortcut_filepath anything_removed=false errors_encountered=false
+
+  for shortcut_filepath in "${D_SHORTCUT_FILEPATHS[@]}"; do
+
+    # Announce attempt
+    debug_print "Attempting to remove shortcut command at: $shortcut_filepath"
+
+    # Remove shortcut, using sudo if need be
+    if [ -w "$( dirname -- "$shortcut_filepath" )" ]; then
+      rm -f "$shortcut_filepath"
+    else
+      sudo rm -f "$shortcut_filepath"
+    fi
+    
+    # Check if removal went fine
+    if [ $? -eq 0 ]; then
+      debug_print \
+        "Successfully removed shortcut command at: $shortcut_filepath"
+      anything_removed=true
+    else
+      debug_print "Failed to remove shortcut command at: $shortcut_filepath"
+      errors_encountered=true
+    fi
+    
+  done
+
+  # Return status
+  if $anything_removed; then
+    if $errors_encountered; then
+      report_failure 'There were problems during removal of shortcut command'
+    else
+      report_success 'Successfully removed shortcut command'
+    fi
+  else
+    if $errors_encountered; then
+      report_failure 'Failed to remove shortcut command'
+    else
+      report_skip 'No shortcut command to remove'
+    fi
+  fi
+}
+
+debug_print()
+{
+  $D_QUIET && return 0
+  printf >&2 "\n${CYAN}%s %s${NORMAL}\n" "==>" "$1"; shift
+  while [ $# -gt 0 ]
+  do printf >&2 "    ${CYAN}%s${NORMAL}\n" "$1"; shift; done; return 0
+}
+
+report_start()
+{
+  $D_QUIET && return 0
+  printf >&2 '\n%s %s\n' "${BOLD}${YELLOW}==>${NORMAL}" "$1"; shift
+  while [ $# -gt 0 ]; do printf >&2 '    %s\n' "$1"; shift; done; return 0
+}
+
+prompt()
+{
+  # Extract predefined answer
+  local predefined_answer="$1"; shift
+
+  # Check predefined answer
+  if [ "$predefined_answer" = true ]; then return 0
+  elif [ "$predefined_answer" = false ]; then return 1
+  fi
+
+  # Extract prompt text
+  local prompt_text="$1"; shift
+
+  # Status variable
   local yes=false
-  printf >&2 '\n%s %s\n  %s\n' \
-    "${BOLD}${YELLOW}==>${NORMAL}" \
-    "${BOLD}Divine.dotfiles${NORMAL} Bash framework installed at:" \
-    "$D_DIR"
-  printf >&2 '\n%s %s\n' \
-    "${BOLD}${RED}${REVERSE} CAREFUL ${NORMAL}" \
+
+  # Print announcement
+  printf >&2 '\n%s %s\n' "${BOLD}${YELLOW}${REVERSE}==>${NORMAL}" "$1"; shift
+  while [ $# -gt 0 ]; do printf >&2 '    %s\n' "$1"; shift; done
+
+  # Print additional uninstallation-specific warning
+  printf >&2 '\n%s %s\n' "${BOLD}${RED}${REVERSE} CAREFUL ${NORMAL}" \
     'This will completely erase installation directory'
-  printf >&2 'Uninstall? [y/n] '
+
+  # Print prompt
+  printf >&2 '\n%s [y/n] ' \
+    "${BOLD}${YELLOW}${REVERSE} ${prompt_text} ${NORMAL}"
 
   # Await answer
   while true; do
@@ -141,29 +273,28 @@ __erase_d_dir()
   printf >&2 '\n'
 
   # Check answer
-  $yes || {
-    printf >&2 '\n%s %s\n' \
-      "${BOLD}${WHITE}==>${NORMAL}" \
-      "Refused to uninstall ${BOLD}Divine.dotfiles${NORMAL}"
-    return 1
-  }
-
-  # Straight-forward enough
-  rm -rf "$D_DIR" || {
-    printf >&2 '\n%s %s\n  %s\n' \
-      "${BOLD}${RED}==>${NORMAL}" \
-      'Failed to erase directory at:' \
-      "$D_DIR"
-    return 1
-  }
-
-  # Report success
-  printf >&2 '\n%s %s %s\n  %s\n' \
-    "${BOLD}${GREEN}==>${NORMAL}" \
-    'Successfully uninstalled' \
-    "${BOLD}Divine.dotfiles${NORMAL} Bash framework from:" \
-    "$D_DIR"
-  return 0
+  if $yes; then return 0; else return 1; fi
 }
 
-main
+report_success()
+{
+  $D_QUIET && return 0
+  printf >&2 '\n%s %s\n' "${BOLD}${GREEN}==>${NORMAL}" "$1"; shift
+  while [ $# -gt 0 ]; do printf >&2 '    %s\n' "$1"; shift; done; return 0
+}
+
+report_skip()
+{
+  $D_QUIET && return 0
+  printf >&2 '\n%s %s\n' "${BOLD}${WHITE}==>${NORMAL}" "$1"; shift
+  while [ $# -gt 0 ]; do printf >&2 '    %s\n' "$1"; shift; done; return 0
+}
+
+report_failure()
+{
+  $D_QUIET && return 0
+  printf >&2 '\n%s %s\n' "${BOLD}${RED}==>${NORMAL}" "$1"; shift
+  while [ $# -gt 0 ]; do printf >&2 '    %s\n' "$1"; shift; done; return 0
+}
+
+main "$@"
