90f9124306b354962484dc7654707c8bd230389f
Grove Pyree <grayarea@protonmail.ch>
2019-05-13 20:37:26 +0300

    Incorporate all printing functions into dprint

--- lib/dprint.utl.sh
+++ lib/dprint.utl.sh
@@ -15,6 +15,40 @@
 #>  dprint_plaque [-nope]… [-c|--color X] [-w|--width X] [-r|--padding-char X] [--] MSG
 #
 
+#>  dprint_debug CHUNKS…
+#
+## If verbose mode is activated, prints message in debug style. Otherwise, does 
+#. nothing.
+#
+## Parameters:
+#.  $@  - Chunks of debug message, to be separated from each other by single 
+#.        space.
+#.        Following special marks are also accepted:
+#.          '-n'  - Insert new line. When used as first argument, inserts new
+#.                  line before any other output.
+#.          '-i'  - Insert new indented line
+#
+## Returns:
+#.  0 - After printing composed message
+#.  1 - If $D_QUIET is set to 'true'
+#
+## Prints:
+#.  stdout  - *nothing*
+#.  stderr  - Composed message, or as little as possible
+#
+dprint_debug()
+{
+  # Check if quiet mode is active
+  [ "$D_QUIET" = true ] && return 1
+
+  # Compose message from arguments and print it all on the go
+  [ "$1" = -n ] && { printf >&2 '\n'; shift; }; printf >&2 '%s' "${CYAN}==>"
+  local chunk; for chunk; do
+    case $chunk in -n) printf >&2 '\n   ';; -i) printf >&2 '\n       ';;
+    *) printf >&2 ' %s' "$chunk";; esac
+  done; printf >&2 '%s\n' "${NORMAL}"; return 0
+}
+
 #>  dprint_msg [-no] [-c|--color X] [--width-N X] [--effects-N X] [--] FIELD1 [FIELD2…]
 #
 ## Prints formatted message consisting of fields. Arguments are textual 
