b2ef4a212d09a345d47513d6aee550ac2c616348
Grove Pyree <grayarea@protonmail.ch>
2019-08-05 18:03:17 +0300

    Slightly improve usage note

--- lib/routines/usage.rtn.sh
+++ lib/routines/usage.rtn.sh
@@ -2,9 +2,9 @@
 #:title:        Divine Bash routine: usage
 #:author:       Grove Pyree
 #:email:        grayarea@protonmail.ch
-#:revnumber:    7
-#:revdate:      2019.07.22
-#:revremark:    New revisioning system
+#:revnumber:    8
+#:revdate:      2019.08.05
+#:revremark:    Slightly improve usage note
 #:created_at:   2018.03.25
 
 ## Part of Divine.dotfiles <https://github.com/no-simpler/divine-dotfiles>
@@ -36,13 +36,13 @@ d__show_usage_and_exit()
 
   local usage_tip
   read -r -d '' usage_tip << EOF
-Usage: ${bold}${D__EXEC_NAME}${normal} ${bold}i${normal}|${bold}install${normal}   [-ynqvewf] [TASK]…  - Launch installation
-   or: ${bold}${D__EXEC_NAME}${normal} ${bold}r${normal}|${bold}remove${normal}    [-ynqvewf] [TASK]…  - Launch removal
-   or: ${bold}${D__EXEC_NAME}${normal} ${bold}c${normal}|${bold}check${normal}     [-ynqvew]  [TASK]…  - Launch checking
+Usage: ${bold}${D__EXEC_NAME}${normal} ${bold}i${normal}|${bold}install${normal}   [-ynqvewf] [TASK]…  - Install deployment(s)
+   or: ${bold}${D__EXEC_NAME}${normal} ${bold}r${normal}|${bold}remove${normal}    [-ynqvewf] [TASK]…  - Uninstall deployment(s)
+   or: ${bold}${D__EXEC_NAME}${normal} ${bold}c${normal}|${bold}check${normal}     [-ynqvew]  [TASK]…  - Check status of deployment(s)
 
-   or: ${bold}${D__EXEC_NAME}${normal} ${bold}a${normal}|${bold}attach${normal}    [-yn]      REPO…    - Add deployment(s) from Github repo
-   or: ${bold}${D__EXEC_NAME}${normal} ${bold}d${normal}|${bold}detach${normal}    [-yn]      REPO…    - Remove previously attached repo
-   or: ${bold}${D__EXEC_NAME}${normal} ${bold}p${normal}|${bold}plug${normal}      [-ynl]     REPO/DIR - Plug Grail from repo or dir
+   or: ${bold}${D__EXEC_NAME}${normal} ${bold}a${normal}|${bold}attach${normal}    [-yn]      REPO…    - Import deployment(s) from Github
+   or: ${bold}${D__EXEC_NAME}${normal} ${bold}d${normal}|${bold}detach${normal}    [-yn]      REPO…    - Discard previously attached repo
+   or: ${bold}${D__EXEC_NAME}${normal} ${bold}p${normal}|${bold}plug${normal}      [-ynl]     REPO/DIR - Plug Grail from repo or local dir
    or: ${bold}${D__EXEC_NAME}${normal} ${bold}u${normal}|${bold}update${normal}    [-yn]      [TASK]…  - Update framework/deployments/Grail
 
    or: ${bold}${D__EXEC_NAME}${normal} --version                       - Show script version
