006c03bbb9b6439f54e5599145d0dce764e69a6e
Grove Pyree <grayarea@protonmail.ch>
2019-08-08 19:31:10 +0300

    Add colorization fallback to escape sequences

--- lib/procedures/print-colors.pcd.sh
+++ lib/procedures/print-colors.pcd.sh
@@ -3,9 +3,9 @@
 #:kind:         global_var
 #:author:       Grove Pyree
 #:email:        grayarea@protonmail.ch
-#:revnumber:    3
-#:revdate:      2019.08.07
-#:revremark:    Grand removal of non-ASCII chars
+#:revnumber:    4
+#:revdate:      2019.08.08
+#:revremark:    Add colorization fallback to escape sequences
 #:created_at:   2018.12.20
 
 ## Part of Divine.dotfiles <https://github.com/no-simpler/divine-dotfiles>
@@ -41,9 +41,9 @@
 #.  $NORMAL                         - (read-only) Stops all of the above
 #
 ## Returns:
-#.  0 - Success
-#.  1 - Terminal does NOT support at least 8 colors
-#.  2 - Some or all of variables are already set
+#.  0 - Color variables are set
+#.  1 - At least one color variable could not be set
+#.  2 - No colorization has been done
 #
 ## Prints:
 #.  stdout: *nothing*
@@ -51,136 +51,224 @@
 #
 d__declare_global_colors()
 {
-  # Storage variable
-  local num_of_colors
+  # Check if tput utility is available
+  if type -P tput &>/dev/null; then
 
-  # Get number of colors supported by terminal
-  if type -P tput &>/dev/null; then num_of_colors=$( tput colors ); fi
+    # Extract number of colors supported
+    local num_of_colors=$( tput colors )
 
-  # Status variable
-  local return_code=0
+    # Check if tput is suitable for colorization
+    if tput sgr0 &>/dev/null \
+      [ -n "$num_of_colors" ] \
+      && [ "$num_of_colors" -ge 8 ]
+    then
 
-  # Check if terminal supports 8+ colors
-  if [ -n "$num_of_colors" ] && [ "$num_of_colors" -ge 8 ]; then
+      # tput is suitable: use it
+      d__colorize_with_tput && return 0 || return 1
 
-    # Coloring output
+    else
 
-    # Set foreground color variables, unless already set
-    [ -z ${BLACK+isset} ] \
-      && readonly   BLACK="$( tput setaf 0 )" || return_code=2
-    [ -z ${RED+isset} ] \
-      && readonly     RED="$( tput setaf 1 )" || return_code=2
-    [ -z ${GREEN+isset} ] \
-      && readonly   GREEN="$( tput setaf 2 )" || return_code=2
-    [ -z ${YELLOW+isset} ] \
-      && readonly  YELLOW="$( tput setaf 3 )" || return_code=2
-    [ -z ${BLUE+isset} ] \
-      && readonly    BLUE="$( tput setaf 4 )" || return_code=2
-    [ -z ${MAGENTA+isset} ] \
-      && readonly MAGENTA="$( tput setaf 5 )" || return_code=2
-    [ -z ${CYAN+isset} ] \
-      && readonly    CYAN="$( tput setaf 6 )" || return_code=2
-    [ -z ${WHITE+isset} ] \
-      && readonly   WHITE="$( tput setaf 7 )" || return_code=2
+      # Unsupported tput, such as on FreeBSD: use escape sequences instead
+      d__colorize_with_escseq && return 0 || return 1
 
-    # Set background color variables, unless already set
-    [ -z ${BG_BLACK+isset} ] \
-      && readonly   BG_BLACK="$( tput setab 0 )" || return_code=2
-    [ -z ${BG_RED+isset} ] \
-      && readonly     BG_RED="$( tput setab 1 )" || return_code=2
-    [ -z ${BG_GREEN+isset} ] \
-      && readonly   BG_GREEN="$( tput setab 2 )" || return_code=2
-    [ -z ${BG_YELLOW+isset} ] \
-      && readonly  BG_YELLOW="$( tput setab 3 )" || return_code=2
-    [ -z ${BG_BLUE+isset} ] \
-      && readonly    BG_BLUE="$( tput setab 4 )" || return_code=2
-    [ -z ${BG_MAGENTA+isset} ] \
-      && readonly BG_MAGENTA="$( tput setab 5 )" || return_code=2
-    [ -z ${BG_CYAN+isset} ] \
-      && readonly    BG_CYAN="$( tput setab 6 )" || return_code=2
-    [ -z ${BG_WHITE+isset} ] \
-      && readonly   BG_WHITE="$( tput setab 7 )" || return_code=2
-
-    # Set effects variables, unless already set
-    [ -z ${BOLD+isset} ] \
-      && readonly     BOLD="$( tput bold )" || return_code=2
-    [ -z ${DIM+isset} ] \
-      && readonly      DIM="$( tput dim )"  || return_code=2
-    [ -z ${ULINE+isset} ] \
-      && readonly    ULINE="$( tput smul )" || return_code=2
-    [ -z ${REVERSE+isset} ] \
-      && readonly  REVERSE="$( tput rev )"  || return_code=2
-    [ -z ${STANDOUT+isset} ] \
-      && readonly STANDOUT="$( tput smso )" || return_code=2
-
-    # Set reset variable, unless already set
-    [ -z ${NORMAL+isset} ] \
-      && readonly NORMAL="$( tput sgr0 )" || return_code=2
+    fi
 
   else
 
-    # Not coloring output
+    # No tput at all, colorize with escape sequences
+    d__colorize_with_escseq && return 0 || return 1
 
-    # Store appropriate return code
-    return_code=1
+    # ...or, don't colorize at all
+    # d__do_not_colorize && return 2 || return 1
+
+  fi
+}
+
+d__colorize_with_tput()
+{
+  # Status variable
+  local all_good=true
+
+  # Set foreground color variables, unless already set
+  [ -z ${BLACK+isset} ] \
+    && readonly   BLACK="$( tput setaf 0 )" || all_good=false
+  [ -z ${RED+isset} ] \
+    && readonly     RED="$( tput setaf 1 )" || all_good=false
+  [ -z ${GREEN+isset} ] \
+    && readonly   GREEN="$( tput setaf 2 )" || all_good=false
+  [ -z ${YELLOW+isset} ] \
+    && readonly  YELLOW="$( tput setaf 3 )" || all_good=false
+  [ -z ${BLUE+isset} ] \
+    && readonly    BLUE="$( tput setaf 4 )" || all_good=false
+  [ -z ${MAGENTA+isset} ] \
+    && readonly MAGENTA="$( tput setaf 5 )" || all_good=false
+  [ -z ${CYAN+isset} ] \
+    && readonly    CYAN="$( tput setaf 6 )" || all_good=false
+  [ -z ${WHITE+isset} ] \
+    && readonly   WHITE="$( tput setaf 7 )" || all_good=false
+
+  # Set background color variables, unless already set
+  [ -z ${BG_BLACK+isset} ] \
+    && readonly   BG_BLACK="$( tput setab 0 )" || all_good=false
+  [ -z ${BG_RED+isset} ] \
+    && readonly     BG_RED="$( tput setab 1 )" || all_good=false
+  [ -z ${BG_GREEN+isset} ] \
+    && readonly   BG_GREEN="$( tput setab 2 )" || all_good=false
+  [ -z ${BG_YELLOW+isset} ] \
+    && readonly  BG_YELLOW="$( tput setab 3 )" || all_good=false
+  [ -z ${BG_BLUE+isset} ] \
+    && readonly    BG_BLUE="$( tput setab 4 )" || all_good=false
+  [ -z ${BG_MAGENTA+isset} ] \
+    && readonly BG_MAGENTA="$( tput setab 5 )" || all_good=false
+  [ -z ${BG_CYAN+isset} ] \
+    && readonly    BG_CYAN="$( tput setab 6 )" || all_good=false
+  [ -z ${BG_WHITE+isset} ] \
+    && readonly   BG_WHITE="$( tput setab 7 )" || all_good=false
+
+  # Set effects variables, unless already set
+  [ -z ${BOLD+isset} ] \
+    && readonly     BOLD="$( tput bold )" || all_good=false
+  [ -z ${DIM+isset} ] \
+    && readonly      DIM="$( tput dim )"  || all_good=false
+  [ -z ${ULINE+isset} ] \
+    && readonly    ULINE="$( tput smul )" || all_good=false
+  [ -z ${REVERSE+isset} ] \
+    && readonly  REVERSE="$( tput rev )"  || all_good=false
+  [ -z ${STANDOUT+isset} ] \
+    && readonly STANDOUT="$( tput smso )" || all_good=false
+
+  # Set reset variable, unless already set
+  [ -z ${NORMAL+isset} ] \
+    && readonly NORMAL="$( tput sgr0 )" || all_good=false
+
+  # Return appropriately
+  $all_good && return 0 || return 1
+}
+
+d__colorize_with_escseq()
+{
+  # Status variable
+  local all_good=true
 
     # Set foreground color variables, unless already set
     [ -z ${BLACK+isset} ] \
-      && readonly   BLACK='' || return_code=2
+      && readonly   BLACK="$( printf "\033[30m" )" || all_good=false
     [ -z ${RED+isset} ] \
-      && readonly     RED='' || return_code=2
+      && readonly     RED="$( printf "\033[31m" )" || all_good=false
     [ -z ${GREEN+isset} ] \
-      && readonly   GREEN='' || return_code=2
+      && readonly   GREEN="$( printf "\033[32m" )" || all_good=false
     [ -z ${YELLOW+isset} ] \
-      && readonly  YELLOW='' || return_code=2
+      && readonly  YELLOW="$( printf "\033[33m" )" || all_good=false
     [ -z ${BLUE+isset} ] \
-      && readonly    BLUE='' || return_code=2
+      && readonly    BLUE="$( printf "\033[34m" )" || all_good=false
     [ -z ${MAGENTA+isset} ] \
-      && readonly MAGENTA='' || return_code=2
+      && readonly MAGENTA="$( printf "\033[35m" )" || all_good=false
     [ -z ${CYAN+isset} ] \
-      && readonly    CYAN='' || return_code=2
+      && readonly    CYAN="$( printf "\033[36m" )" || all_good=false
     [ -z ${WHITE+isset} ] \
-      && readonly   WHITE='' || return_code=2
+      && readonly   WHITE="$( printf "\033[97m" )" || all_good=false
 
     # Set background color variables, unless already set
     [ -z ${BG_BLACK+isset} ] \
-      && readonly   BG_BLACK='' || return_code=2
+      && readonly   BG_BLACK="$( printf "\033[40m" )" || all_good=false
     [ -z ${BG_RED+isset} ] \
-      && readonly     BG_RED='' || return_code=2
+      && readonly     BG_RED="$( printf "\033[41m" )" || all_good=false
     [ -z ${BG_GREEN+isset} ] \
-      && readonly   BG_GREEN='' || return_code=2
+      && readonly   BG_GREEN="$( printf "\033[42m" )" || all_good=false
     [ -z ${BG_YELLOW+isset} ] \
-      && readonly  BG_YELLOW='' || return_code=2
+      && readonly  BG_YELLOW="$( printf "\033[43m" )" || all_good=false
     [ -z ${BG_BLUE+isset} ] \
-      && readonly    BG_BLUE='' || return_code=2
+      && readonly    BG_BLUE="$( printf "\033[44m" )" || all_good=false
     [ -z ${BG_MAGENTA+isset} ] \
-      && readonly BG_MAGENTA='' || return_code=2
+      && readonly BG_MAGENTA="$( printf "\033[45m" )" || all_good=false
     [ -z ${BG_CYAN+isset} ] \
-      && readonly    BG_CYAN='' || return_code=2
+      && readonly    BG_CYAN="$( printf "\033[46m" )" || all_good=false
     [ -z ${BG_WHITE+isset} ] \
-      && readonly   BG_WHITE='' || return_code=2
+      && readonly   BG_WHITE="$( printf "\033[107m" )" || all_good=false
 
     # Set effects variables, unless already set
     [ -z ${BOLD+isset} ] \
-      && readonly     BOLD='' || return_code=2
+      && readonly     BOLD="$( printf "\033[1m" )" || all_good=false
     [ -z ${DIM+isset} ] \
-      && readonly      DIM='' || return_code=2
+      && readonly      DIM="$( printf "\033[2m" )" || all_good=false
     [ -z ${ULINE+isset} ] \
-      && readonly    ULINE='' || return_code=2
+      && readonly    ULINE="$( printf "\033[4m" )" || all_good=false
     [ -z ${REVERSE+isset} ] \
-      && readonly  REVERSE='' || return_code=2
+      && readonly  REVERSE="$( printf "\033[7m" )" || all_good=false
     [ -z ${STANDOUT+isset} ] \
-      && readonly STANDOUT='' || return_code=2
+      && readonly STANDOUT="$( printf "\033[5m" )" || all_good=false
 
     # Set reset variable, unless already set
     [ -z ${NORMAL+isset} ] \
-      && readonly NORMAL='' || return_code=2
+      && readonly NORMAL="$( printf "\033[0m" )" || all_good=false
 
-  fi
+  # Return appropriately
+  $all_good && return 0 || return 1
+}
 
-  # Report
-  return $return_code
+d__do_not_colorize()
+{
+  # Status variable
+  local all_good=true
+
+    # Set foreground color variables, unless already set
+    [ -z ${BLACK+isset} ] \
+      && readonly   BLACK='' || all_good=false
+    [ -z ${RED+isset} ] \
+      && readonly     RED='' || all_good=false
+    [ -z ${GREEN+isset} ] \
+      && readonly   GREEN='' || all_good=false
+    [ -z ${YELLOW+isset} ] \
+      && readonly  YELLOW='' || all_good=false
+    [ -z ${BLUE+isset} ] \
+      && readonly    BLUE='' || all_good=false
+    [ -z ${MAGENTA+isset} ] \
+      && readonly MAGENTA='' || all_good=false
+    [ -z ${CYAN+isset} ] \
+      && readonly    CYAN='' || all_good=false
+    [ -z ${WHITE+isset} ] \
+      && readonly   WHITE='' || all_good=false
+
+    # Set background color variables, unless already set
+    [ -z ${BG_BLACK+isset} ] \
+      && readonly   BG_BLACK='' || all_good=false
+    [ -z ${BG_RED+isset} ] \
+      && readonly     BG_RED='' || all_good=false
+    [ -z ${BG_GREEN+isset} ] \
+      && readonly   BG_GREEN='' || all_good=false
+    [ -z ${BG_YELLOW+isset} ] \
+      && readonly  BG_YELLOW='' || all_good=false
+    [ -z ${BG_BLUE+isset} ] \
+      && readonly    BG_BLUE='' || all_good=false
+    [ -z ${BG_MAGENTA+isset} ] \
+      && readonly BG_MAGENTA='' || all_good=false
+    [ -z ${BG_CYAN+isset} ] \
+      && readonly    BG_CYAN='' || all_good=false
+    [ -z ${BG_WHITE+isset} ] \
+      && readonly   BG_WHITE='' || all_good=false
+
+    # Set effects variables, unless already set
+    [ -z ${BOLD+isset} ] \
+      && readonly     BOLD='' || all_good=false
+    [ -z ${DIM+isset} ] \
+      && readonly      DIM='' || all_good=false
+    [ -z ${ULINE+isset} ] \
+      && readonly    ULINE='' || all_good=false
+    [ -z ${REVERSE+isset} ] \
+      && readonly  REVERSE='' || all_good=false
+    [ -z ${STANDOUT+isset} ] \
+      && readonly STANDOUT='' || all_good=false
+
+    # Set reset variable, unless already set
+    [ -z ${NORMAL+isset} ] \
+      && readonly NORMAL='' || all_good=false
+
+  # Return appropriately
+  $all_good && return 0 || return 1
 }
 
 d__declare_global_colors
 unset -f d__declare_global_colors
+unset -f d__colorize_with_tput
+unset -f d__colorize_with_escseq
+unset -f d__do_not_colorize
