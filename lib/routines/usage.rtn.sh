f80103d4debe95175c5de9f82cc9d1ceec947e14
Grove Pyree <grayarea@protonmail.ch>
2019-09-01 20:17:26 +0300

    Prettify usage message more

--- lib/routines/usage.rtn.sh
+++ lib/routines/usage.rtn.sh
@@ -2,9 +2,9 @@
 #:title:        Divine Bash routine: usage
 #:author:       Grove Pyree
 #:email:        grayarea@protonmail.ch
-#:revnumber:    13
+#:revnumber:    14
 #:revdate:      2019.09.01
-#:revremark:    Tweak bolding in miscellaneous locations
+#:revremark:    Prettify usage message more
 #:created_at:   2018.03.25
 
 ## Part of Divine.dotfiles <https://github.com/no-simpler/divine-dotfiles>
@@ -39,18 +39,19 @@ d__show_usage_and_exit()
 
   local usage_tip
   read -r -d '' usage_tip << EOF
-${bold}$D__FMWK_NAME usage${normal}:
-Check status of dpls:   $ ${bold}${D__EXEC_NAME}${normal} ${bold}c${normal}|${bold}check${normal}   [-ynqvew]  [-b BUNDLE]... [TASK]...
-Install dpls:           $ ${bold}${D__EXEC_NAME}${normal} ${bold}i${normal}|${bold}install${normal} [-ynqvewf] [-b BUNDLE]... [TASK]...
-Uninstall dpls:         $ ${bold}${D__EXEC_NAME}${normal} ${bold}r${normal}|${bold}remove${normal}  [-ynqvewf] [-b BUNDLE]... [TASK]...
+${bold}${D__FMWK_NAME}${normal} usage:
 
-Attach bundles:         $ ${bold}${D__EXEC_NAME}${normal} ${bold}a${normal}|${bold}attach${normal}  [-yn]                     REPO...
-Detach bundles:         $ ${bold}${D__EXEC_NAME}${normal} ${bold}d${normal}|${bold}detach${normal}  [-yn]                     REPO...
-Plug Grail:             $ ${bold}${D__EXEC_NAME}${normal} ${bold}p${normal}|${bold}plug${normal}    [-ynl]                    REPO/DIR...
-Update fmwk/dpls/Grail: $ ${bold}${D__EXEC_NAME}${normal} ${bold}u${normal}|${bold}update${normal}  [-yn]                     [TASK]...
+  ${bold}${D__EXEC_NAME}${normal} ${bold}c${normal}|${bold}check${normal}   [-ynqvew]  [-b BUNDLE]... [TASK]...  - Check dpls
+  ${bold}${D__EXEC_NAME}${normal} ${bold}i${normal}|${bold}install${normal} [-ynqvewf] [-b BUNDLE]... [TASK]...  - Install dpls
+  ${bold}${D__EXEC_NAME}${normal} ${bold}r${normal}|${bold}remove${normal}  [-ynqvewf] [-b BUNDLE]... [TASK]...  - Uninstall dpls
 
-Show framework version: $ ${bold}${D__EXEC_NAME}${normal} --version
-Show help summary:      $ ${bold}${D__EXEC_NAME}${normal} -h|--help
+  ${bold}${D__EXEC_NAME}${normal} ${bold}a${normal}|${bold}attach${normal}  [-yn]   REPO...                      - Attach bundles
+  ${bold}${D__EXEC_NAME}${normal} ${bold}d${normal}|${bold}detach${normal}  [-yn]   REPO...                      - Detach bundles
+  ${bold}${D__EXEC_NAME}${normal} ${bold}p${normal}|${bold}plug${normal}    [-ynl]  REPO/DIR...                  - Plug Grail
+  ${bold}${D__EXEC_NAME}${normal} ${bold}u${normal}|${bold}update${normal}  [-yn]   [fmwk|dpls|grail]...         - Update parts
+
+  ${bold}${D__EXEC_NAME}${normal} --version                                      - Show version
+  ${bold}${D__EXEC_NAME}${normal} -h|--help                                      - Show help
 EOF
 
   # Print usage tip
