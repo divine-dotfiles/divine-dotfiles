abfaeb5205823d529a2283b664e18e6996aaba5e
Grove Pyree <grayarea@protonmail.ch>
2019-09-01 18:01:04 +0300

    Make usage note narrower

--- lib/routines/usage.rtn.sh
+++ lib/routines/usage.rtn.sh
@@ -2,9 +2,9 @@
 #:title:        Divine Bash routine: usage
 #:author:       Grove Pyree
 #:email:        grayarea@protonmail.ch
-#:revnumber:    11
+#:revnumber:    12
 #:revdate:      2019.09.01
-#:revremark:    Add --bundle opt to reference materials
+#:revremark:    Make usage note narrower
 #:created_at:   2018.03.25
 
 ## Part of Divine.dotfiles <https://github.com/no-simpler/divine-dotfiles>
@@ -36,17 +36,18 @@ d__show_usage_and_exit()
 
   local usage_tip
   read -r -d '' usage_tip << EOF
-Usage: ${bold}${D__EXEC_NAME}${normal} ${bold}c${normal}|${bold}check${normal}   [-ynqvew]  [-b BUNDLE]... [TASK]...  - Check status of deployment(s)
-   or: ${bold}${D__EXEC_NAME}${normal} ${bold}i${normal}|${bold}install${normal} [-ynqvewf] [-b BUNDLE]... [TASK]...  - Install deployment(s)
-   or: ${bold}${D__EXEC_NAME}${normal} ${bold}r${normal}|${bold}remove${normal}  [-ynqvewf] [-b BUNDLE]... [TASK]...  - Uninstall deployment(s)
+${bold}$D__FMWK_NAME usage${normal}:
+Check status of dpls:   $ ${bold}${D__EXEC_NAME}${normal} ${bold}c${normal}|${bold}check${normal}   [-ynqvew]  [-b BUNDLE]... [TASK]...
+Install dpls:           $ ${bold}${D__EXEC_NAME}${normal} ${bold}i${normal}|${bold}install${normal} [-ynqvewf] [-b BUNDLE]... [TASK]...
+Uninstall dpls:         $ ${bold}${D__EXEC_NAME}${normal} ${bold}r${normal}|${bold}remove${normal}  [-ynqvewf] [-b BUNDLE]... [TASK]...
 
-   or: ${bold}${D__EXEC_NAME}${normal} ${bold}a${normal}|${bold}attach${normal}  [-yn]                     REPO...    - Import deployment(s) from Github
-   or: ${bold}${D__EXEC_NAME}${normal} ${bold}d${normal}|${bold}detach${normal}  [-yn]                     REPO...    - Discard previously attached repo
-   or: ${bold}${D__EXEC_NAME}${normal} ${bold}p${normal}|${bold}plug${normal}    [-ynl]                    REPO/DIR   - Plug Grail from repo or local dir
-   or: ${bold}${D__EXEC_NAME}${normal} ${bold}u${normal}|${bold}update${normal}  [-yn]                     [TASK]...  - Update framework/deployments/Grail
+Attach bundles:         $ ${bold}${D__EXEC_NAME}${normal} ${bold}a${normal}|${bold}attach${normal}  [-yn]                     REPO...
+Detach bundles:         $ ${bold}${D__EXEC_NAME}${normal} ${bold}d${normal}|${bold}detach${normal}  [-yn]                     REPO...
+Plug Grail:             $ ${bold}${D__EXEC_NAME}${normal} ${bold}p${normal}|${bold}plug${normal}    [-ynl]                    REPO/DIR...
+Update fmwk/dpls/Grail: $ ${bold}${D__EXEC_NAME}${normal} ${bold}u${normal}|${bold}update${normal}  [-yn]                     [TASK]...
 
-   or: ${bold}${D__EXEC_NAME}${normal} --version                                      - Show framework version
-   or: ${bold}${D__EXEC_NAME}${normal} -h|--help                                      - Show help summary
+Show framework version: $ ${bold}${D__EXEC_NAME}${normal} --version
+Show help summary:      $ ${bold}${D__EXEC_NAME}${normal} -h|--help
 EOF
 
   # Print usage tip
