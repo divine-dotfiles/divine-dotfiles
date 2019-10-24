de763016b4bc083ccabeaadd644fa2700cd7e423
Grove Pyree <grayarea@protonmail.ch>
2019-10-24 13:16:05 +0300

    Be more verbose by default when detaching

--- lib/routines/detach.rtn.sh
+++ lib/routines/detach.rtn.sh
@@ -2,8 +2,8 @@
 #:title:        Divine Bash routine: detach
 #:author:       Grove Pyree
 #:email:        grayarea@protonmail.ch
-#:revdate:      2019.10.17
-#:revremark:    Split prep-gh in two
+#:revdate:      2019.10.24
+#:revremark:    Be more verbose by default when detaching
 #:created_at:   2019.06.28
 
 ## Part of Divine.dotfiles <https://github.com/no-simpler/divine-dotfiles>
@@ -103,7 +103,8 @@ d___detach_bundle()
 
   # Compose destination path; print location
   bdst="$D__DIR_BUNDLES/$barg"
-  d__notify -q -- "Location: $bdst"
+  d__notify -l -- "Repo URL: https://github.com/$barg"
+  d__notify -l -- "Location: $bdst"
 
   # Check if such bundle directory exists
   if [ -e "$bdst" ]; then
