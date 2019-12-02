754c6bf6974bef2cefce3092f6ab6817b40d2259
Grove Pyree <grayarea@protonmail.ch>
2019-12-02 09:06:14 +0300

    Add temp output in fmwk install

--- lib/routines/fmwk-install.rtn.sh
+++ lib/routines/fmwk-install.rtn.sh
@@ -3,7 +3,7 @@
 #:author:       Grove Pyree
 #:email:        grayarea@protonmail.ch
 #:revdate:      2019.12.02
-#:revremark:    Ensure no empty template is left after failed fmwk inst.
+#:revremark:    Add temp output in fmwk install
 #:created_at:   2019.10.15
 
 ## Part of Divine.dotfiles <https://github.com/divine-dotfiles/divine-dotfiles>
@@ -204,9 +204,12 @@ d___pfc_shortcut()
       continue
     fi
     if [ -e "$sdir/$snm" ]; then
+      echo "Something exists at '$sdir/$snm'"
       d__notify -- "Skipping candidate '$sdir'" \
         "(file named '$snm' already exists in it)"
       continue
+    else
+      echo "Absolutely nothing exists at '$sdir/$snm', even though it very much DOES"
     fi
     if ! [ -w "$sdir" ]; then
       nwrd+=("$sdir")
